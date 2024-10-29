4eaa9ea7a5b1def9a6774990177f17a9ad81b674
santoshi kc <santoshikc04@gmail.com>
2024-10-29 21:48:01 +0545

    use import statement instead of const variable

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const port = process.env.PORT || 5000;
+
+
+import express from 'express';
+import cors from 'cors';
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 
