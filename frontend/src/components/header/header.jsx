1cdf9e425c0041f88be2aa157947ce7b9948881d
santoshi kc <santoshikc04@gmail.com>
2024-11-09 14:51:06 +0545

    default correction in header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
-import './header.css'
+import React, { useState } from 'react';
+import './header.css';
 import Foodie from '../../assets/logo/Foodie.png';
 import User from '../../assets/icons/user.svg';
 import Cart from '../../assets/icons/cart.svg';
 
+const Header = () => {
+  const [menu, setMenu] = useState('menu');
 
 
-function Header() {
   return (
     <header className="bg-orange-500 text-white py-4 px-8 flex justify-between items-center">
       <a className='logo' href="#"><img src={Foodie} alt="" /></a>
+      <div className="pages">
+        <ul>
+          <li onClick={() => setMenu('Home')} className={menu === 'Home' ? 'active' : ''}>Home</li>
+          <li onClick={() => setMenu('About-Us')} className={menu === 'About-Us' ? 'active' : ''}>About-Us</li>
+          <li onClick={() => setMenu('Contact-Us')} className={menu === 'Contact-Us' ? 'active' : ''}>Contact-Us</li>
+        </ul>
+      </div>
       <div className='link-wrapper'>
-      <a href="#"> Order </a>
-      <a href="#"><img src={Cart} className='icon' alt=""/>Cart </a>
-      <a href="#"><img src={User} className='icon' alt=""/> Login </a>
+        {/* <a href="#"> Order </a> */}
+        <a href="#"><img src={Cart} className='icon' alt="" /> <div className="dot"></div> </a>
+        <a href="#"><img src={User} className='icon' alt="" /> Login </a>
       </div>
     </header>
-  );
+  )
 }
 
 export default Header;
