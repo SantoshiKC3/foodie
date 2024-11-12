afc6cfbb40c63233870d703f0085a134c3847912
santoshi kc <santoshikc04@gmail.com>
2024-11-12 20:06:38 +0545

    added the foodphoto

--- frontend/src/assets/images/images.js
+++ frontend/src/assets/images/images.js
@@ -1,11 +1,59 @@
 import banner_food from './banner_food.png'
 import banner_img from './banner_img.jpg'
 
+import food_1 from './food_1.jpg'
+import food_2 from './food_2.jpg'
+import food_3 from './food_3.jpg'
+import food_4 from './food_4.jpg'
+import food_5 from './food_5.png';
+import food_6 from './food_6.png';
+import food_7 from './food_7.png';
+import food_8 from './food_8.png';
+import food_9 from './food_9.png';
+import food_10 from './food_10.png';
+import food_11 from './food_11.png';
+import food_12 from './food_12.png';
+import food_13 from './food_13.png';
+import food_14 from './food_14.png';
+import food_15 from './food_15.png';
+import food_16 from './food_16.png';
+import food_17 from './food_17.png';
+import food_18 from './food_18.png';
+import food_19 from './food_19.png';
+import food_20 from './food_20.png';
+import food_21 from './food_21.png';
+import food_22 from './food_22.png';
+import food_23 from './food_23.png';
+import food_24 from './food_24.png';
 
 
 
 
 export const images = {
-banner_food,
-banner_img
+    banner_food,
+    banner_img,
+    food_1,
+    food_2,
+    food_3,
+    food_4,
+    food_5,
+    food_6,
+    food_7,
+    food_8,
+    food_9,
+    food_10,
+    food_11,
+    food_12,
+    food_13,
+    food_14,
+    food_15,
+    food_16,
+    food_17,
+    food_18,
+    food_19,
+    food_20,
+    food_21,
+    food_22,
+    food_23,
+    food_24,
 }
